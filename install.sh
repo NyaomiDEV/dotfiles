25f3c5b061c2e385ab1a32831875a69ef4b74e31
Naomi Calabretta <nyaomicalabretta@gmail.com>
2024-07-12 18:26:29 +0200

    a bit of a refactor really

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
+
 if [ "$(whoami)" != "naomi" ]; then
 	echo "This user is not naomi. It is not advised to use this script if you are not her, as it *WILL* add her personal configuration to your system."
 	echo "If this is what you want, then modify this script by hand so that you can use it."
@@ -9,8 +11,8 @@ fi
 
 echo "Naomi's Dotfiles!"
 
-packages="git zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-fast-syntax-highlighting"
-optional_packages="cod fzf bat ttf-nerd-fonts-symbols"
+packages="git zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-fast-syntax-highlighting eza fzf rga bat atuin ttf-nerd-fonts-symbols"
+optional_packages="cod"
 
 git_packages="fzf-tab su-zsh-plugin"
 typeset -A sources
@@ -30,31 +32,42 @@ done
 
 if [ -n "$not_found" ]; then
 	echo "---"
-	echo "[!] Missing packages: $not_found"
+	echo "[!] Missing packages:$not_found"
 	echo "[!] Please install them to ensure correct dotfiles functionality!"
 	exit 1
 fi
 
 if [ -n "$optional_not_found" ]; then
 	echo "---"
-	echo "[!] Missing optional packages: $optional_not_found"
+	echo "[!] Missing optional packages:$optional_not_found"
 	echo "[!] Please install them to have the full experience!"
 fi
 
-mkdir -p $HOME/.zsh/plugins 2>/dev/null
+mkdir -p "$HOME/.zsh/plugins" 2>/dev/null
 
 for package in $git_packages; do
-	[ ! -d "$HOME/.zsh/plugins/$package" ] && git clone "${sources[$package]}" "$HOME/.zsh/plugins/$package"
+	if [ ! -d "$HOME/.zsh/plugins/$package" ]; then
+		while
+			echo -n "[?] Do you want to retrieve $package from Git? [Y/n]: "
+			read answer
+			if [ "$answer" = "n" ] || [ "$answer" = "N" ]; then
+				echo "[-] Skipping $package"
+				continue 2
+			fi
+			[ -n "$answer" ] && [ "$answer" != "y" ] && [ "$answer" != "Y" ]
+		do :; done
+
+		git clone "${sources[$package]}" "$HOME/.zsh/plugins/$package"
+	fi
 done
 
 a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; BASEDIR=$(cd "$a"; pwd -P)
 
-files=$(find "$BASEDIR" -type f \
+files="$(find "$BASEDIR" -type f \
+	-not -path "$BASEDIR/.gitignore" \
 	-not -path "$BASEDIR/.git/*" \
 	-not -path "$BASEDIR/install.sh" \
-	-not -path "$BASEDIR/install_zsh_only.sh" \
-	-not -path "$BASEDIR/install_zsh_only_mac.sh" \
-	-not -path "$BASEDIR/README.md")
+	-not -path "$BASEDIR/README.md")"
 
 oldIFS=$IFS
 IFS=$'\n'
@@ -62,9 +75,13 @@ IFS=$'\n'
 for file in $files; do
 	echo "---"
 	relative=$(realpath --relative-to="$BASEDIR" $file)
+	destination="$HOME/$relative"
+	if beginswith "dot-" "$relative"; then
+		destination="$HOME/.${relative:4}"
+	fi
 	
 	while
-		echo -n "[?] Do you want to install $relative ? [Y/n]: "
+		echo -n "[?] Do you want to install $relative to $destination? [Y/n]: "
 		read answer
 		if [ "$answer" = "n" ] || [ "$answer" = "N" ]; then
 			echo "[-] Skipping $relative"
@@ -73,30 +90,17 @@ for file in $files; do
 		[ -n "$answer" ] && [ "$answer" != "y" ] && [ "$answer" != "Y" ]
 	do :; done
 	
-	echo "[+] Setting up $relative"
-	mkdir -p $(dirname "$HOME/$relative") 2>/dev/null
-	if ([ -f "$HOME/$relative" ] && [ ! -L "$HOME/$relative" ]) || ([ -e "$HOME/$relative" ] && [ "$(realpath "$HOME/$relative")" != "$file" ]); then
-		echo "[-] Backing up your $relative to $relative.old"
-		mv "$HOME/$relative" "$HOME/$relative.old"
+	if ([ -f "$destination" ] && [ ! -L "$destination" ]) || ([ -e "$destination" ] && [ "$(realpath "$destination")" != "$file" ]); then
+		echo "[-] Backing up $destination to $destination.old"
+		mv "$destination" "$destination.old"
 	fi
-	ln -s "$file" "$HOME/$relative" 2>/dev/null
+	echo "[+] Setting up $destination"
+	mkdir -p "$(dirname "$destination")" 2>/dev/null
+	[ -L "$destination" ] && rm "$destination" 2>/dev/null
+	ln -s "$file" "$destination" 2>/dev/null
 done
 
 IFS=$oldIFS
 echo "---"
-
-while
-	echo "[!] The script can make some extra work that needs to be done manually otherwise."
-	echo "[!] Please refer to the source to know what it does."
-	echo -n "[?] Do you want the script to do the extra work for you? [Y/n]: "
-	read answer
-	if [ "$answer" = "n" ] || [ "$answer" = "N" ]; then
-		echo "[-] Skipping the extra work"
-		exit
-	fi
-	[ -n "$answer" ] && [ "$answer" != "y" ] && [ "$answer" != "Y" ]
-do :; done
-
-echo "[!] Doing the extra work now"
 
 echo "[!] All done!"
